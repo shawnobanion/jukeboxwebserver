da0e55562eb3bc8992cee1f59355f0d14bb61eb8
Shawn O'Banion <shawn.obanion@gmail.com>
2011-11-09 08:58:58 -0600

    more udpates to views

--- web/jukebox/views.py
+++ web/jukebox/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 import json
 from pymongo import Connection, objectid
+from django.views.decorators.csrf import csrf_exempt
 
 _con = Connection('jukebox-shawnobanion-db-0.dotcloud.com', 13105)
 _db = _con['admin']
@@ -16,10 +17,14 @@ def enqueue_song(request, event_id, song_id):
     
 def get_event_queue(request, event_id):
     event = _db.events.find_one({ '_id' : objectid.ObjectId(event_id) })
-    return HttpResponse(json.dumps(event['queue']), mimetype="application/json")
-    
+    return HttpResponse(json.dumps(event['queue'] if 'queue' in event else ''), mimetype="application/json")
+
+@csrf_exempt
 def create_event(request):
-    return HttpResponse('Success!')
+    if request.method == 'POST':
+        _db.events.insert(json.loads(request.raw_post_data))        
+        return HttpResponse('Event added.')
+    return HttpResponse('No event added.')
 
 def get_events(request):
     events = [{ 'id' : str(event['_id']), 'name' : event['name'] } for event in _db.events.find()]
